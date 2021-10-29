0a3d6763554b7df339a9c464b2ec4a9cc65fd6b0
tjcos3 <tjcos3@naver.com>
2021-10-29 18:29:43 +0900

    sql도 같이올라가라

---
+++ PhoneBook.sql
@@ -0,0 +1,27 @@
+/*****
+JDBC - PhoneBook.java
+*****/
+
+create table phonebook_tb (
+    name varchar2(30) primary key,
+    phoneNumber varchar2(20),
+    birthday date 
+);
+
+select * from tab;
+desc phonebook_tb;
+
+create sequence seq_phonebook
+    increment by 1
+    start with 1
+    minvalue 1
+    nomaxvalue
+    nocycle
+    nocache;
+
+select * from seq_phonebook;
+
+commit;
+
+
+
